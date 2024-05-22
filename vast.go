ee5891ccdad5692f33202165a97de946747d84ca
milos-pesic-zattoo <54900657+milos-pesic-zattoo@users.noreply.github.com>
2024-05-22 19:36:35 +0200

    Update vast.go

--- vast.go
+++ vast.go
@@ -5,6 +5,8 @@ import "encoding/xml"
 
 // VAST is the root <VAST> tag
 type VAST struct {
+	// root tag name (VAST)
+	XMLName xml.Name `xml:"VAST"`
 	// The version of the VAST spec (should be either "2.0" or "3.0")
 	Version string `xml:"version,attr" json:",omitempty"`
 	// One or more Ad elements. Advertisers and video content publishers may
